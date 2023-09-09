c884c4af2f7b75748622d5986f80f256327dd78b
ahmed353ch <117953320+ahmed353ch@users.noreply.github.com>
2023-09-09 07:37:55 +0300

    Update azan2.py
    Signed-off-by: ahmed353ch <117953320+ahmed353ch@users.noreply.github.com>

--- AnonXMusic/plugins/play/azan2.py
+++ AnonXMusic/plugins/play/azan2.py
@@ -14,46 +14,15 @@ from pyrogram.errors import FloodWait
 from pyrogram.raw import types
 import random
 
-chat = []
-
-async def get_prayer_times(address, method, school):
-    url = f"http://api.aladhan.com/timingsByAddress?address={address}&method={method}&school={school}"
-    response = requests.get(url)
-    data = response.json()
-    return data["data"]["timings"]
-
-async def azan2():
-    address = "Cairo"
-    method = 4  
-    school = 0  
-    prayer_times = await get_prayer_times(address, method, school)
-    times_message = f"أوقات الصلاة في {address}:\n"
-    times_message += f"الفجر: {prayer_times['Fajr']}\n"
-    times_message += f"الشروق: {prayer_times['Sunrise']}\n"
-    times_message += f"الظهر: {prayer_times['Dhuhr']}\n"
-    times_message += f"العصر: {prayer_times['Asr']}\n"
-    times_message += f"المغرب: {prayer_times['Maghrib']}\n"
-    times_message += f"العشاء: {prayer_times['Isha']}\n"
-
-async def azan(message):
-
+@app.on_message(filters.regex("^اذن$"))
+async def strcall(client, message):
     assistant = await group_assistant(Anony, message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./AnonXMusic/assets/azan.mp3"), stream_type=StreamType().pulse_stream)
-        text = "حان الآن موعد الأذان:"
-        await message.reply(text)
-        await asyncio.sleep(60)
-        await assistant.leave_group_call(message.chat.id)
+        await assistant.join_group_call(message.chat.id, AudioPiped(".assets/azan.mp3"), stream_type=StreamType().pulse_stream)
     except NoActiveGroupCall:
-        await message.reply("المكالمة الجماعية مغلقة.")
+        await message.reply(f"الكول مقفول اصلا يصاحبي")
+    except TelegramServerError:
+        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام")
     except AlreadyJoinedError:
-        text = "حان الآن موعد الأذان:"
-        await message.reply(text)
-    await asyncio.sleep(60)
-    for i in chat:
-        try:
-            await app.send_message(i, random.choice(azan2))
-        except:
-            pass
+        pass
 
-asyncio.create_task(azan2())
